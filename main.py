c7ceb58e3950ddce31ad139734462606832e158d
yura307 <yurafenchishin@gmail.com>
2026-08-27 18:41:55 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import random
 import asyncio
+import requests
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
@@ -17,6 +18,25 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+# --- НАЛАШТУВАННЯ TELEGRAM БОТА ---
+TELEGRAM_TOKEN = "8441433958:AAFwOMhZ2ubBHsEzctJnU_1NBiJi_iI5JGo"
+TELEGRAM_CHAT_ID = "632883207"
+
+def send_telegram_alert(message_text):
+    if TELEGRAM_TOKEN == "ВАШ_ТОКЕН_ТУТ":
+        return # Захист, якщо ви забули вписати токен
+    url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
+    payload = {
+        "chat_id": TELEGRAM_CHAT_ID,
+        "text": message_text,
+        "parse_mode": "HTML"
+    }
+    try:
+        requests.post(url, json=payload, timeout=3)
+    except Exception as e:
+        print(f"Помилка відправки в TG: {e}")
+# ----------------------------------
 
 MONGO_URL = os.getenv("MONGO_URL")
 
@@ -93,12 +113,25 @@ def update_profile(data: UserProfileUpdate):
 async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
     t = 0.0
+    alert_active = False # Запобіжник від спаму в Telegram
+    
     try:
         while True:
             vibro = abs(random.gauss(30, 5) if random.random() > 0.1 else random.uniform(50, 85))
             sound = 65 + (random.random() * 15)
             temp = 42.0 + (random.random() * 1.5)
             rssi = int(-75 + (random.random() * 15))
+            
+            # --- ЛОГІКА ВІДПРАВКИ ТРИВОГИ В TELEGRAM ---
+            if vibro > 75 and not alert_active:
+                alert_active = True
+                msg = f"🚨 <b>УВАГА! Критична вібрація!</b>\nПоказник: {round(vibro, 1)} мм/с\nОбладнання: Верстат ЧПК"
+                asyncio.create_task(asyncio.to_thread(send_telegram_alert, msg))
+                
+            elif vibro <= 70:
+                alert_active = False # Скидаємо запобіжник
+            # -------------------------------------------
+
             await websocket.send_json({"vibro": round(vibro, 1), "sound": round(sound, 1), "temp": round(temp, 1), "rssi": rssi, "timestamp": t})
             await asyncio.sleep(0.3)
     except WebSocketDisconnect:
